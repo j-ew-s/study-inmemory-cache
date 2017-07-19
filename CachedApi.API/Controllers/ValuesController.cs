f362497b62786f5d710b0e91a995d7bb6a6d4a9c
j-ew-s <gabriel.scavassa@outlook.com>
2017-07-18 22:50:11 -0300

    Initial: Adding initial solution

---
+++ CachedApi.API/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CachedApi.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly IMemoryCache _cache;
+
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _cache = memoryCache;
+        }
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            string cacheKey = "UpdatedTime";
+            DateTime cacheEntry;
+
+            if (!_cache.TryGetValue(cacheKey, out cacheEntry))
+            {
+                cacheEntry = DateTime.Now;
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(3.00));
+
+                _cache.Set(cacheKey, cacheEntry, cacheEntryOptions);
+            }
+
+            return cacheEntry.ToString();
+        }
+    }
+}
